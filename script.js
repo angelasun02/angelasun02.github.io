93177ed466d9b888ac2777d41cbba37e87a7467c
ysun07 <ysun07@risd.edu>
2021-10-22 14:57:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,17 @@
                 $( "h2" ).click(function() {
                     $("body").toggleClass('on');
                 });
+
+               /*if($('body').hasClass('.a') ) {
+                    this.removeClass('.a, .b, .c');
+                    this.addClass('.b');
+                }；*/
+                //state A(.a), state B(.b), state C(.c)
+                // if body has a class .a, if I click h2, I want:
+                // - body.removeClass('.a, .b, .c');
+                // -body.addClass('.b');
+                // if body has a class .b, if I click h2, I want:
+                // - remove all classes like above^
+                // -body.addClass('.c');
+
                 });
