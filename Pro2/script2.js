6ffc2aeaafe90671b273ad9fc91171165fe7e4a7
ysun07 <ysun07@risd.edu>
2021-10-28 23:25:39 -0400

    Update script2.js

--- Pro2/script2.js
+++ Pro2/script2.js
@@ -1,2 +1,7 @@
-console.log('working.');
-
+$(document).ready(function(){
+  $("a").hover(function(){
+    $("h2021").css("display", "block");
+    }, function(){
+    $("h2021").css("display", "block");
+  });
+});
