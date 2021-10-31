13ee11c04585730aec0eab541ff0b81c3656f89a
ysun07 <ysun07@risd.edu>
2021-10-30 21:55:21 -0400

    Update script2.js

--- Pro2/script2.js
+++ Pro2/script2.js
@@ -5,3 +5,15 @@ $(document).ready(function(){
     $(".h2021").css("display", "block");
   });
 });
+
+$(document).ready(function(){
+  $("a").hover(function(){
+    $("a").css("font-weight","400");
+    $("a").css("color","#A00000");
+    $("a").css("opacity","0.8");
+    }, function(){
+    $("a").css("font-weight","400");
+    $("a").css("color","#A00000");
+    $("a").css("opacity","0.8");
+  });
+});
