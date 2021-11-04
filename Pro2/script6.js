e771ee121b4de8d60f64d66ecf49d1bd0cfa11ff
ysun07 <ysun07@risd.edu>
2021-11-03 23:44:06 -0400

    Update script6.js

--- Pro2/script6.js
+++ Pro2/script6.js
@@ -1,17 +1,15 @@
 var now = new Date();
 var hour = now.getHours();
 var minute = now.getMinutes();
-if(hour <= 8 && minute <= 30){window.location.href='Break.html';}
-else if(hour <= 9 && minute <= 30){window.location.href='MariaAntelman.html';}
-else if(hour <= 10 && minute <= 30){window.location.href='SohrabHura.html';}
-else if(hour <= 11 && minute <= 30){window.location.href='DavidAlekhuogie.html';}
-else if(hour <= 13 && minute <= 30){window.location.href='Break.html';}
-else if(hour <= 14 && minute <= 30){window.location.href='ÖzlemAltın.html';}
-else if(hour <= 15 && minute <= 30){window.location.href='ZoraJMurff.html';}
-else if(hour <= 16 && minute <= 30){window.location.href='IrinaRozovsky.html';}
-else if(hour <= 17 && minute <= 30){window.location.href='IñakiBonillas.html';}
-else if(hour <= 18 && minute <= 30){window.location.href='DionneLee.html';}
-else if(hour >= 18 && minute >= 30){window.location.href='Break.html';}
+if(8 <= hour <= 9 && minute <= 30){window.location.href='MariaAntelman.html';}
+if(hour <= 10 && minute <= 30){window.location.href='SohrabHura.html';}
+if(hour <= 11 && minute <= 30){window.location.href='DavidAlekhuogie.html';}
+if(hour <= 14 && minute <= 30){window.location.href='ÖzlemAltın.html';}
+if(hour <= 15 && minute <= 30){window.location.href='ZoraJMurff.html';}
+if(hour <= 16 && minute <= 30){window.location.href='IrinaRozovsky.html';}
+if(hour <= 17 && minute <= 30){window.location.href='IñakiBonillas.html';}
+if(hour <= 18 && minute <= 30){window.location.href='DionneLee.html';}
+else {window.location.href='IrinaRozovsky.html';}
 
 //for image link only 
 $(document).ready(function(){
