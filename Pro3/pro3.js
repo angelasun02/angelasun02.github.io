a089a9220c7fa8eca70288b57d663bdf6e0cd576
ysun07 <ysun07@risd.edu>
2021-11-19 10:14:48 -0500

    Create pro3.js

---
+++ Pro3/pro3.js
@@ -0,0 +1,155 @@
+            $(document).ready(function(){
+                $( ".star5" ).click(function() {
+                    $("body").toggleClass('darkmode');   
+                });
+                $( ".colorgroup1" ).click(function() {
+                    $(".colorgroup1").css('display','none');
+                    $(".colorgroup2").css('display','none');
+                    $(".colorgroup3").css('display','none');
+                    $(".colorgroup4").css('display','none');
+                    $(".colorgroup5").css('display','none');
+                    $(".colorgroup1on").css('display','inline-block');
+                });
+                $( ".colorgroup1on" ).click(function() {
+                    $(".colorgroup1on").css('display','none');
+                    $(".colorgroup1").css('display','inline-block');
+                    $(".colorgroup2").css('display','inline-block');
+                    $(".colorgroup3").css('display','inline-block');
+                    $(".colorgroup4").css('display','inline-block');
+                    $(".colorgroup5").css('display','inline-block');
+                });
+                $( ".colorgroup2" ).click(function() {
+                    $(".colorgroup1").css('display','none');
+                    $(".colorgroup2").css('display','none');
+                    $(".colorgroup3").css('display','none');
+                    $(".colorgroup4").css('display','none');
+                    $(".colorgroup5").css('display','none');
+                    $(".colorgroup2on").css('display','inline-block');
+                    $(".header").css('color','#FF7A00');
+                    $(".title").css('color','#FF3D00');
+                    $(".line1").css('background-color','#FF7A00');
+                    $(".line2").css('background-color','#FF7A00');
+                    $(".line3").css('background-color','#FF7A00');
+                    $(".line4").css('background-color','#FF7A00');
+                    $(".star5").css('color','#FF7A00');
+                    $(".star6").css('color','#FF7A00');
+                });
+                $( ".colorgroup2on" ).click(function() {
+                    $(".colorgroup2on").css('display','none');
+                    $(".colorgroup1").css('display','inline-block');
+                    $(".colorgroup2").css('display','inline-block');
+                    $(".colorgroup3").css('display','inline-block');
+                    $(".colorgroup4").css('display','inline-block');
+                    $(".colorgroup5").css('display','inline-block');
+                    $(".header").css('color','#52FF00');
+                    $(".title").css('color','#00FF38');
+                    $(".line1").css('background-color','#00FF38');
+                    $(".line2").css('background-color','#00FF38');
+                    $(".line3").css('background-color','#00FF38');
+                    $(".line4").css('background-color','#00FF38');
+                    $(".star5").css('color','#00FF38');
+                    $(".star6").css('color','#00FF38');
+                });
+                $( ".colorgroup3" ).click(function() {
+                    $(".colorgroup1").css('display','none');
+                    $(".colorgroup2").css('display','none');
+                    $(".colorgroup3").css('display','none');
+                    $(".colorgroup4").css('display','none');
+                    $(".colorgroup5").css('display','none');
+                    $(".colorgroup3on").css('display','inline-block');
+                    $(".header").css('color','#EB00FF');
+                    $(".title").css('color','#9E00FF');
+                    $(".line1").css('background-color','#EB00FF');
+                    $(".line2").css('background-color','#EB00FF');
+                    $(".line3").css('background-color','#EB00FF');
+                    $(".line4").css('background-color','#EB00FF');
+                    $(".star5").css('color','#EB00FF');
+                    $(".star6").css('color','#EB00FF');
+                });
+                $( ".colorgroup3on" ).click(function() {
+                    $(".colorgroup3on").css('display','none');
+                    $(".colorgroup1").css('display','inline-block');
+                    $(".colorgroup2").css('display','inline-block');
+                    $(".colorgroup3").css('display','inline-block');
+                    $(".colorgroup4").css('display','inline-block');
+                    $(".colorgroup5").css('display','inline-block');
+                    $(".header").css('color','#52FF00');
+                    $(".title").css('color','#00FF38');
+                    $(".line1").css('background-color','#00FF38');
+                    $(".line2").css('background-color','#00FF38');
+                    $(".line3").css('background-color','#00FF38');
+                    $(".line4").css('background-color','#00FF38');
+                    $(".star5").css('color','#00FF38');
+                    $(".star6").css('color','#00FF38');
+                });
+                $( ".colorgroup4" ).click(function() {
+                    $(".colorgroup1").css('display','none');
+                    $(".colorgroup2").css('display','none');
+                    $(".colorgroup3").css('display','none');
+                    $(".colorgroup4").css('display','none');
+                    $(".colorgroup5").css('display','none');
+                    $(".colorgroup4on").css('display','inline-block');
+                    $(".header").css('color','#FFC700');
+                    $(".title").css('color','#FF8A00');
+                    $(".line1").css('background-color','#FF8A00');
+                    $(".line2").css('background-color','#FF8A00');
+                    $(".line3").css('background-color','#FF8A00');
+                    $(".line4").css('background-color','#FF8A00');
+                    $(".star5").css('color','#FF8A00');
+                    $(".star6").css('color','#FF8A00');
+                });
+                $( ".colorgroup4on" ).click(function() {
+                    $(".colorgroup4on").css('display','none');
+                    $(".colorgroup1").css('display','inline-block');
+                    $(".colorgroup2").css('display','inline-block');
+                    $(".colorgroup3").css('display','inline-block');
+                    $(".colorgroup4").css('display','inline-block');
+                    $(".colorgroup5").css('display','inline-block');
+                    $(".header").css('color','#52FF00');
+                    $(".title").css('color','#00FF38');
+                    $(".line1").css('background-color','#00FF38');
+                    $(".line2").css('background-color','#00FF38');
+                    $(".line3").css('background-color','#00FF38');
+                    $(".line4").css('background-color','#00FF38');
+                    $(".star5").css('color','#00FF38');
+                    $(".star6").css('color','#00FF38');
+                });
+                $( ".colorgroup5" ).click(function() {
+                    $(".colorgroup1").css('display','none');
+                    $(".colorgroup2").css('display','none');
+                    $(".colorgroup3").css('display','none');
+                    $(".colorgroup4").css('display','none');
+                    $(".colorgroup5").css('display','none');
+                    $(".colorgroup5on").css('display','inline-block');
+                    $(".header").css('color','#00D1FF');
+                    $(".title").css('color','#BDFF00');
+                    $(".line1").css('background-color','#00D1FF');
+                    $(".line2").css('background-color','#00D1FF');
+                    $(".line3").css('background-color','#00D1FF');
+                    $(".line4").css('background-color','#00D1FF');
+                    $(".star5").css('color','#00D1FF');
+                    $(".star6").css('color','#00D1FF');
+                });
+                $( ".colorgroup5on" ).click(function() {
+                    $(".colorgroup5on").css('display','none');
+                    $(".colorgroup1").css('display','inline-block');
+                    $(".colorgroup2").css('display','inline-block');
+                    $(".colorgroup3").css('display','inline-block');
+                    $(".colorgroup4").css('display','inline-block');
+                    $(".colorgroup5").css('display','inline-block');
+                    $(".header").css('color','#52FF00');
+                    $(".title").css('color','#00FF38');
+                    $(".line1").css('background-color','#00FF38');
+                    $(".line2").css('background-color','#00FF38');
+                    $(".line3").css('background-color','#00FF38');
+                    $(".line4").css('background-color','#00FF38');
+                    $(".star5").css('color','#00FF38');
+                    $(".star6").css('color','#00FF38');
+                });
+                $( ".group1" ).click(function() {
+                $(".group1").toggleClass('.container1');
+                });
+                });
+                
+
+                
