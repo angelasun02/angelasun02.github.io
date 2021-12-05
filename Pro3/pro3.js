dd022937d1db88acda2e88011e68016325ac103b
ysun07 <ysun07@risd.edu>
2021-12-05 17:45:51 -0500

    Update pro3.js

--- Pro3/pro3.js
+++ Pro3/pro3.js
@@ -5,6 +5,7 @@
                 $( ".star6" ).click(function() {
                     $("body").toggleClass('darkmode');   
                 });
+
                 $( ".colorgroup1" ).click(function() {
                     $(".colorgroup1").css('display','none');
                     $(".colorgroup2").css('display','none');
@@ -12,6 +13,8 @@
                     $(".colorgroup4").css('display','none');
                     $(".colorgroup5").css('display','none');
                     $(".colorgroup1on").css('display','inline-block');
+                    $(".colorbg1").toggle('inline-block');
+                    $(".colorbg21").toggle('inline-block');
                     $(".header").css('color','#52FF00');
                     $(".title").css('color','#00FF38');
                     $(".line1").css('background-color','#52FF00');
@@ -20,7 +23,40 @@
                     $(".line4").css('background-color','#52FF00');
                     $(".star5").css('color','#52FF00');
                     $(".star6").css('color','#52FF00');
+                $(".container1" ).click(function() {
+                    $(".container1").toggleClass('container11');
+                    $(".circle").toggle('inline-block');
+                    $(".circle1").toggle('inline-block');
                 });
+                $( ".container2" ).click(function() {
+                    $(".container2").toggleClass('container21');
+                    $(".square").toggle('inline-block');
+                    $(".square1").toggle('inline-block');
+                });
+                $( ".container3" ).click(function() {
+                    $(".container3").toggleClass('container31');
+                    $(".linee").toggle('inline-block');
+                    $(".linee1").toggle('inline-block');
+                });
+                $( ".container4" ).click(function() {
+                    $(".container4").toggleClass('container41');
+                    $(".polygon").toggle('inline-block');
+                    $(".polygon1").toggle('inline-block');
+                });
+                $( ".container5" ).click(function() {
+                    $(".container5").toggleClass('container51');
+                    $(".ellipse").toggle('inline-block');
+                    $(".ellipse1").toggle('inline-block');
+                });
+                $( ".refresh" ).hover(function() {
+                    $(".refresh").toggleClass('refresh1');
+                });
+                $( ".generate" ).click(function() {
+                    $(".generate").toggleClass('generate1');
+                });
+                });
+
+
                 $( ".colorgroup1on" ).click(function() {
                     $(".colorgroup1on").css('display','none');
                     $(".colorgroup1").css('display','inline-block');
@@ -29,6 +65,7 @@
                     $(".colorgroup4").css('display','inline-block');
                     $(".colorgroup5").css('display','inline-block');
                 });
+
                 $( ".colorgroup2" ).click(function() {
                     $(".colorgroup1").css('display','none');
                     $(".colorgroup2").css('display','none');
@@ -36,6 +73,8 @@
                     $(".colorgroup4").css('display','none');
                     $(".colorgroup5").css('display','none');
                     $(".colorgroup2on").css('display','inline-block');
+                    $(".colorbg1").toggle('inline-block');
+                    $(".colorbg22").toggle('inline-block');
                     $(".header").css('color','#FF7A00');
                     $(".title").css('color','#FF3D00');
                     $(".line1").css('background-color','#FF7A00');
@@ -44,7 +83,39 @@
                     $(".line4").css('background-color','#FF7A00');
                     $(".star5").css('color','#FF7A00');
                     $(".star6").css('color','#FF7A00');
+                $(".container1" ).click(function() {
+                    $(".container1").toggleClass('container12');
+                    $(".circle2").toggle('inline-block');
+                    $(".circle12").toggle('inline-block');
                 });
+                $( ".container2" ).click(function() {
+                    $(".container2").toggleClass('container22');
+                    $(".square2").toggle('inline-block');
+                    $(".square12").toggle('inline-block');
+                });
+                $( ".container3" ).click(function() {
+                    $(".container3").toggleClass('container32');
+                    $(".linee2").toggle('inline-block');
+                    $(".linee12").toggle('inline-block');
+                });
+                $( ".container4" ).click(function() {
+                    $(".container4").toggleClass('container42');
+                    $(".polygon2").toggle('inline-block');
+                    $(".polygon12").toggle('inline-block');
+                });
+                $( ".container5" ).click(function() {
+                    $(".container5").toggleClass('container52');
+                    $(".ellipse2").toggle('inline-block');
+                    $(".ellipse12").toggle('inline-block');
+                });
+                $( ".refresh" ).hover(function() {
+                    $(".refresh").toggleClass('refresh2');
+                });
+                $( ".generate" ).click(function() {
+                    $(".generate").toggleClass('generate2');
+                });
+                });
+
                 $( ".colorgroup2on" ).click(function() {
                     $(".colorgroup2on").css('display','none');
                     $(".colorgroup1").css('display','inline-block');
@@ -53,6 +124,7 @@
                     $(".colorgroup4").css('display','inline-block');
                     $(".colorgroup5").css('display','inline-block');
                 });
+
                 $( ".colorgroup3" ).click(function() {
                     $(".colorgroup1").css('display','none');
                     $(".colorgroup2").css('display','none');
@@ -60,6 +132,8 @@
                     $(".colorgroup4").css('display','none');
                     $(".colorgroup5").css('display','none');
                     $(".colorgroup3on").css('display','inline-block');
+                    $(".colorbg1").toggle('inline-block');
+                    $(".colorbg23").toggle('inline-block');
                     $(".header").css('color','#EB00FF');
                     $(".title").css('color','#9E00FF');
                     $(".line1").css('background-color','#EB00FF');
@@ -68,7 +142,39 @@
                     $(".line4").css('background-color','#EB00FF');
                     $(".star5").css('color','#EB00FF');
                     $(".star6").css('color','#EB00FF');
+                $(".container1" ).click(function() {
+                    $(".container1").toggleClass('container13');
+                    $(".circle3").toggle('inline-block');
+                    $(".circle13").toggle('inline-block');
                 });
+                $( ".container2" ).click(function() {
+                    $(".container2").toggleClass('container23');
+                    $(".square3").toggle('inline-block');
+                    $(".square13").toggle('inline-block');
+                });
+                $( ".container3" ).click(function() {
+                    $(".container3").toggleClass('container33');
+                    $(".linee3").toggle('inline-block');
+                    $(".linee13").toggle('inline-block');
+                });
+                $( ".container4" ).click(function() {
+                    $(".container4").toggleClass('container43');
+                    $(".polygon3").toggle('inline-block');
+                    $(".polygon13").toggle('inline-block');
+                });
+                $( ".container5" ).click(function() {
+                    $(".container5").toggleClass('container53');
+                    $(".ellipse3").toggle('inline-block');
+                    $(".ellipse13").toggle('inline-block');
+                });
+                $( ".refresh" ).hover(function() {
+                    $(".refresh").toggleClass('refresh3');
+                });
+                $( ".generate" ).click(function() {
+                    $(".generate").toggleClass('generate3');
+                });
+                });
+
                 $( ".colorgroup3on" ).click(function() {
                     $(".colorgroup3on").css('display','none');
                     $(".colorgroup1").css('display','inline-block');
@@ -77,6 +183,7 @@
                     $(".colorgroup4").css('display','inline-block');
                     $(".colorgroup5").css('display','inline-block');
                 });
+
                 $( ".colorgroup4" ).click(function() {
                     $(".colorgroup1").css('display','none');
                     $(".colorgroup2").css('display','none');
@@ -84,6 +191,8 @@
                     $(".colorgroup4").css('display','none');
                     $(".colorgroup5").css('display','none');
                     $(".colorgroup4on").css('display','inline-block');
+                    $(".colorbg1").toggle('inline-block');
+                    $(".colorbg24").toggle('inline-block');
                     $(".header").css('color','#FFC700');
                     $(".title").css('color','#FF8A00');
                     $(".line1").css('background-color','#FFC700');
@@ -92,7 +201,39 @@
                     $(".line4").css('background-color','#FFC700');
                     $(".star5").css('color','#FFC700');
                     $(".star6").css('color','#FFC700');
+                $(".container1" ).click(function() {
+                    $(".container1").toggleClass('container14');
+                    $(".circle4").toggle('inline-block');
+                    $(".circle14").toggle('inline-block');
                 });
+                $( ".container2" ).click(function() {
+                    $(".container2").toggleClass('container24');
+                    $(".square4").toggle('inline-block');
+                    $(".square14").toggle('inline-block');
+                });
+                $( ".container3" ).click(function() {
+                    $(".container3").toggleClass('container34');
+                    $(".linee4").toggle('inline-block');
+                    $(".linee14").toggle('inline-block');
+                });
+                $( ".container4" ).click(function() {
+                    $(".container4").toggleClass('container44');
+                    $(".polygon4").toggle('inline-block');
+                    $(".polygon14").toggle('inline-block');
+                });
+                $( ".container5" ).click(function() {
+                    $(".container5").toggleClass('container54');
+                    $(".ellipse4").toggle('inline-block');
+                    $(".ellipse14").toggle('inline-block');
+                });
+                $( ".refresh" ).hover(function() {
+                    $(".refresh").toggleClass('refresh4');
+                });
+                $( ".generate" ).click(function() {
+                    $(".generate").toggleClass('generate4');
+                });
+                });
+
                 $( ".colorgroup4on" ).click(function() {
                     $(".colorgroup4on").css('display','none');
                     $(".colorgroup1").css('display','inline-block');
@@ -101,6 +242,7 @@
                     $(".colorgroup4").css('display','inline-block');
                     $(".colorgroup5").css('display','inline-block');
                 });
+
                 $( ".colorgroup5" ).click(function() {
                     $(".colorgroup1").css('display','none');
                     $(".colorgroup2").css('display','none');
@@ -108,6 +250,8 @@
                     $(".colorgroup4").css('display','none');
                     $(".colorgroup5").css('display','none');
                     $(".colorgroup5on").css('display','inline-block');
+                    $(".colorbg1").toggle('inline-block');
+                    $(".colorbg25").toggle('inline-block');
                     $(".header").css('color','#00D1FF');
                     $(".title").css('color','#BDFF00');
                     $(".line1").css('background-color','#00D1FF');
@@ -116,7 +260,39 @@
                     $(".line4").css('background-color','#00D1FF');
                     $(".star5").css('color','#00D1FF');
                     $(".star6").css('color','#00D1FF');
+                $(".container1" ).click(function() {
+                    $(".container1").toggleClass('container15');
+                    $(".circle5").toggle('inline-block');
+                    $(".circle15").toggle('inline-block');
                 });
+                $( ".container2" ).click(function() {
+                    $(".container2").toggleClass('container25');
+                    $(".square5").toggle('inline-block');
+                    $(".square15").toggle('inline-block');
+                });
+                $( ".container3" ).click(function() {
+                    $(".container3").toggleClass('container35');
+                    $(".linee5").toggle('inline-block');
+                    $(".linee15").toggle('inline-block');
+                });
+                $( ".container4" ).click(function() {
+                    $(".container4").toggleClass('container45');
+                    $(".polygon5").toggle('inline-block');
+                    $(".polygon15").toggle('inline-block');
+                });
+                $( ".container5" ).click(function() {
+                    $(".container5").toggleClass('container55');
+                    $(".ellipse5").toggle('inline-block');
+                    $(".ellipse15").toggle('inline-block');
+                });
+                $( ".refresh" ).hover(function() {
+                    $(".refresh").toggleClass('refresh5');
+                });
+                $( ".generate" ).click(function() {
+                    $(".generate").toggleClass('generate5');
+                });
+                });
+
                 $( ".colorgroup5on" ).click(function() {
                     $(".colorgroup5on").css('display','none');
                     $(".colorgroup1").css('display','inline-block');
@@ -125,31 +301,17 @@
                     $(".colorgroup4").css('display','inline-block');
                     $(".colorgroup5").css('display','inline-block');
                 });
-                $( ".container1" ).click(function() {
-                    $(".container1").toggleClass('container11');
-                });
-                $( ".container2" ).click(function() {
-                    $(".container2").toggleClass('container21');
-                });
-                $( ".container3" ).click(function() {
-                    $(".container3").toggleClass('container31');
-                });
-                $( ".container4" ).click(function() {
-                    $(".container4").toggleClass('container41');
-                });
-                $( ".container5" ).click(function() {
-                    $(".container5").toggleClass('container51');
-                });
-                $( ".refresh" ).hover(function() {
-                    $(".refresh").toggleClass('refresh1');
-                });
+
+
+                
+
+
+
                 $( ".refresh" ).click(function() {
                         console.log('clicked');
                         window.location.reload(true);
                 });
-                $( ".generate" ).click(function() {
-                    $(".generate").toggleClass('generate1');
-                });
+                
                 $( ".text1" ).click(function() {
                     $(".type1").css('display','none');
                 });
